c5c4d010c5520afe1ef7476f2a0fafe91daf6dd4
Anik-36 <delighteraranik@gmail.com>
2024-05-02 22:13:09 +0600

    problem solving

---
+++ A_Problemsolving_Log.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long 
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int t ;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        char s[n+1];
+        int ct = 0;
+        int hsh[n+1] = {0};
+        for(int i=1; i<=n; i++){
+            cin>>s[i];
+            hsh[int(s[i])-64]++;
+        }
+        for(int i=1; i<=n; i++){
+            if(hsh[i]>=i){
+                ct++;
+            }
+        }
+        cout<<ct<<endl;
+    }
+}
