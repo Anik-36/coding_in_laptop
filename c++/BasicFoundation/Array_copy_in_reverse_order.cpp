6e25a56b86de894b26ea49742b2bba771a2eec42
Anik-36 <delighteraranik@gmail.com>
2024-05-09 17:23:45 +0600

    reverse array reserve

--- c++/BasicFoundation/Array_copy_in_reverse_order.cpp
+++ c++/BasicFoundation/Array_copy_in_reverse_order.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 int main(){
     int a[10] = {1,2,3,4,5,6,7,8,9,10};
-    int a2[10];
+    int a2[10] ;
     for(int i = 0 , j=9; i<10 ; i++,j--){
-        a[j] = a[i];
+        a2[j] = a[i];
     }
     for(int i=0 ; i<10 ; i++){
         a[i] = a2[i];
     }
-    for(int i= 0 ; i<9 ; i++){
+    for(int i= 0 ; i<=9 ; i++){
         cout<<a[i]<<" ";
     }
 }
