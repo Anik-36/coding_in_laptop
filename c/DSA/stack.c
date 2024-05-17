bd50be1cb35ec87ef162e8e2f65480fc46f3f349
Anik-36 <delighteraranik@gmail.com>
2024-05-17 23:41:26 +0600

    make stack rearrnge the code

--- c/DSA/stack.c
+++ c/DSA/stack.c
@@ -7,98 +7,98 @@ int push(int stack[max],int *top, int *data);
 
 void main(){
 
-int stack[max], data;
-int top, option, n;
-top =-1;
+    int stack[max], data;
+    int top, option, n;
+    top =-1;
 
-do{
-    printf("***Stack Menu***");
-    printf("\n 1. push");
-    printf("\n 2. pop");
-    printf("\n 3. Display");
-    printf("\n 4. Exit");
-    printf("\n Select proper option: ");
-    scanf("%d", &option);
+    do{
+        printf("***Stack Menu***");
+        printf("\n 1. push");
+        printf("\n 2. pop");
+        printf("\n 3. Display");
+        printf("\n 4. Exit");
+        printf("\n Select proper option: ");
+        scanf("%d", &option);
 
-    switch(option){
-case 1:
-    printf("\n Enter a value:");
-    scanf("%d", &data);
-    n = push(stack, &top, &data);
-    if( n==max){
-        printf("\nStack is full!!!");
-    }else{
-    printf("\nPushed value= %d\n", data);
-    }
-    break;
+        switch(option){
+    case 1:
+        printf("\n Enter a value:");
+        scanf("%d", &data);
+        n = push(stack, &top, &data);
+        if( n==max){
+            printf("\nStack is full!!!");
+        }else{
+        printf("\nPushed value= %d\n", data);
+        }
+        break;
 
-    case 2:
-    n = pop(stack, &top, &data);
-    if( n== -1){
-        printf("\nStack is empty!!!");
-    }else{
-    printf("\nPoped value= %d\n", data);
-    }
-    break;
+        case 2:
+        n = pop(stack, &top, &data);
+        if( n== -1){
+            printf("\nStack is empty!!!");
+        }else{
+        printf("\nPoped value= %d\n", data);
+        }
+        break;
 
-     case 3:
-    n = display(stack, &top, &data);
-    break;
+        case 3:
+        n = display(stack, &top, &data);
+        break;
 
-     case 4:
-        exit(0);
+        case 4:
+            exit(0);
 
-        default: printf("\nWrong Choice!!!! chose between 1 to 4.");
+            default: printf("\nWrong Choice!!!! chose between 1 to 4.");
 
 
-    }
-}while(1);
+        }
+    }while(1);
 
 }
 
 
 int push(int stack[max],int *top, int *data){
 
-if(*top == max - 1){
-    return(-1);
-}
-else{
-    *top = *top + 1;
-    stack[*top] = *data;
-    return (1);
-}
+    if(*top == max - 1){
+        return(-1);
+    }
+    else{
+        *top = *top + 1;
+        stack[*top] = *data;
+        return (1);
+    }
 }
 
 
 int pop(int stack[max],int *top, int *data){
-if(*top == -1){
-    return(-1);
-}
-else{
-    *data = stack[*top];
-    *top = *top - 1;
-    return(1);
-}
+    if(*top == -1){
+        return(-1);
+    }
+    else{
+        *data = stack[*top];
+        *top = *top - 1;
+        return(1);
+    }
 }
 
 
 int display(int stack[max], int *top, int *data){
 
-int i;
-if(*top == -1){
-    printf("\nStack if empty!!!");
-
-}
-else{
-    printf("\nStack is: ");
-    for(i = *top; i>=0; --i){
-        printf("%d", stack[i]);
+    int i;
+    if(*top == -1){
+        printf("\nStack if empty!!!");
 
     }
-    printf("\nPress any key....");
+    else{
+        printf("\nStack is: ");
+        for(i = *top; i>=0; --i){
+            printf("%d", stack[i]);
+
+        }
+        printf("\nPress any key....");
 
 
-}
+    }
 
 
 }
