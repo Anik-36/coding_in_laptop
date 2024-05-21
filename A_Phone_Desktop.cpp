3e94369c7a7113c90cd7bb6d8cc46a26cf10d8be
Anik-36 <delighteraranik@gmail.com>
2024-05-21 12:43:34 +0600

    contest problem solved

--- A_Phone_Desktop.cpp
+++ A_Phone_Desktop.cpp
@@ -8,26 +8,33 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int a,b , ct = 0 , e = 0 ,c = 0, g , s;
+        int a,b , ct = 0 , c = 0, g , s , h , i = 0;
         cin>>a>>b;
-        if(b!=0){
-            c = b/2;
-            if(b%2>0){
-                c++;
-            }
+        c = b/2;
+        if(b%2>0){
+            c++;
+        }
+        h  =  a + b*4;
+        i = h/15;
+        if(h%15>0){
+            i++;
+        }
+        if(i>c){
+            cout<<i<<endl;
+        }else{
             s = c*15;
             g = b*4;
             g+=a;
             if(g>s){
                 c++;
             }
+            cout<<c<<endl;
         }
-        else{
-            c = a/15;
-            if(a%15>0){
-                c++;
-            }
-        }
-        cout<<c<<endl;
+        // if( b == 0 ){
+        //     c = a/15;
+        //     if(a%15>0){
+        //         c++;
+        //     }
+        // }
     }
 }
