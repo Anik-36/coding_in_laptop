4c19963821e7adaac8fc09fc99e449d56503a53b
Anik-36 <delighteraranik@gmail.com>
2024-05-30 14:35:56 +0600

    somethign changes happenes

--- A_Bazoka_and_Mocha_s_Array.cpp
+++ A_Bazoka_and_Mocha_s_Array.cpp
@@ -5,5 +5,17 @@
 #define no cout<<"NO"<<endl;
 using namespace std;
 int main(){
-    
+    int n;
+    cin>>n;
+    int a[n];
+    int x[n] , y[n];
+    for(int i = 0 ; i<n; i++){
+        cin>>a[i];
+    }
+    for( int  i = 0 ; i<n; i++){
+        if(a[i]>a[i+1]){
+            x[i] = a[i];
+        }
+        
+    }
 }
