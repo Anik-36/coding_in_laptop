4b7c4bf8859fb06e4b769e02ec8dbebfdd50e44b
Anik-36 <delighteraranik@gmail.com>
2024-07-03 20:07:40 +0600

    different approach

--- c/phitron/array/reversing_array.c
+++ c/phitron/array/reversing_array.c
@@ -9,6 +9,19 @@ int main(){
     for(int i = 0; i<n; i++){
         printf("%d ",a[i]);
     }
+
+
+    // int a2[n];
+    // for(int i = 0 ; i<n; i++){
+    //     a2[i] = a[n-1-i];
+    // }
+    // printf("\n");
+    // for(int i = 0; i<n; i++){
+    //     printf("%d ",a2[i]);
+    // }
+
+
+    
     for(int i = 0 , j = n-1; i<j; i++,j--){
         int temp = a[i];
         a[i] = a[j];
