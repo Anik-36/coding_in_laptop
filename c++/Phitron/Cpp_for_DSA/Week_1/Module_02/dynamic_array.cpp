406bd67dfc1cd065af64b873d28f6fe2fad60fa8
Anik-36 <delighteraranik@gmail.com>
2024-10-29 22:17:09 +0600

    dynamic array alocating

---
+++ c++/Phitron/Cpp_for_DSA/Week_1/Module_02/dynamic_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long 
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int *a = new int[5];
+    for(int i = 0 ; i<5; i++){
+        cin>>a[i];
+    }
+    for(int i = 0 ;i<5; i++){
+        cout<<a[i]<<" ";
+    }
+}
