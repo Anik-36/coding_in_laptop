1adbd0581452fabc3a0adf87069731d935e412e8
Anik-36 <delighteraranik@gmail.com>
2024-11-01 17:02:55 +0600

    practicing dynamic allocation in c++

--- c++/BasicFoundation/hello.cpp
+++ c++/BasicFoundation/hello.cpp
@@ -5,9 +5,5 @@
 #define no cout<<"NO"<<endl;
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    if(n>2 and n%2==0) yes
-    else no
-    //cout<<"hello world"<<endl;
+    cout<<"Hello Set-up"<<endl;
 }
