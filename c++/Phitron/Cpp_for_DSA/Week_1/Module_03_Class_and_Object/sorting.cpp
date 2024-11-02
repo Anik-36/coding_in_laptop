33ec7a9860e13f756b9da179068581a993a5cc6a
Anik-36 <delighteraranik@gmail.com>
2024-11-02 15:11:21 +0600

    practising

---
+++ c++/Phitron/Cpp_for_DSA/Week_1/Module_03_Class_and_Object/sorting.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long 
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int n; cin>>n;
+    int a[n] ;
+    for(int i = 0 ; i<n; i++){
+        cin>>a[i];
+    }
+
+    for(int i = 0 ; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    // sorting function assending order
+    sort(a,a+n);
+    for(int i = 0 ; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    // for dessending order
+    sort(a,a+n,greater<int>());
+    for(int i = 0 ; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+}
