dc411c2f9a42a628bfd61db9cf8f1ce7891a4d00
Anik-36 <delighteraranik@gmail.com>
2024-11-06 22:29:38 +0600

    assuit O-Sorting sovled

--- O_Sort_String.cpp
+++ O_Sort_String.cpp
@@ -7,20 +7,36 @@ using namespace std;
 int main(){
     int n; 
     cin>>n;
-    string s;
-    cin>>s;
-    int f[26]={0};
-    for(int i = 0; i<n; i++){
-        f[s[i]-'a']++;
+    char c;
+    int f[26] = {0};
+    while(cin>>c){
+        f[c-'a']++;
     }
-    for(int i = 0; i<26; i++){
-        if(f[i]>0){
-            while(f[i]--){
-                cout<<(char)(i+'a');
-            }
+    for(int i = 0 ; i<26; i++){
+        while(f[i]>0){
+            cout<<char(i+'a');
+            f[i]--;
         }
     }
-    cout<<endl;
+
+
+
+
+
+    // string s;
+    // cin>>s;
+    // int f[26]={0};
+    // for(int i = 0; i<n; i++){
+    //     f[s[i]-'a']++;
+    // }
+    // for(int i = 0; i<26; i++){
+    //     if(f[i]>0){
+    //         while(f[i]--){
+    //             cout<<(char)(i+'a');
+    //         }
+    //     }
+    // }
+    // cout<<endl;
 
     //sort(s.begin(),s.end());
     //cout<<s<<endl;
