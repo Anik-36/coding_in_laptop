f0edbf1491584071e273b5d1b8510898daec289e
Anik-36 <delighteraranik@gmail.com>
2024-11-28 22:20:55 +0600

    rever array

---
+++ c/phitron/array/Module_9/reversing_using_2_pointer_tech.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i = 0 ; i<n; i++){
+        scanf("%d",&a[i]);
+    }
+    int i = 0;
+    int j = n-1;
+    while(i<j){
+        int tmp  = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+        i++;
+        j--;
+    }
+    for(int i = 0 ; i<n; i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
