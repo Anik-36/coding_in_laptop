a76c81219359fd89449ae91987ad6b011786a588
Anik-36 <delighteraranik@gmail.com>
2024-12-11 23:14:18 +0600

    solved

--- c/phitron/string/Module 11/String_copy.c
+++ c/phitron/string/Module 11/String_copy.c
@@ -3,7 +3,7 @@
 int main(){
     char a[100], b[100];
     scanf("%s%s",&a,&b);
-    for(int i = 0; i<strlen(b); i++){
+    for(int i = 0; i<=strlen(b); i++){
         a[i] = b[i];
     }
     // a[strlen(b)] = '\0';
