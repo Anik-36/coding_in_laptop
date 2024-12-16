8f3ab6d0708d72265d9893648004aea49befa804
Anik-36 <delighteraranik@gmail.com>
2024-12-16 15:18:40 +0600

    div 4 c solved " hard problem "

---
+++ C_Hard_Problem.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int m,a,b,c;
+        cin>>m>>a>>b>>c;
+        int s = 0 ;
+        if(a<=m){
+            s+=a;
+        }else{
+            a = a-m;
+            s+=a;
+        }
+        if(b<=m){
+            s+=b;
+        }else{
+            b = b-m;
+            s+=b;
+        }
+        if(((m*2)-s)>=c){
+            s+=c;
+        }else{
+            s+=((m*2)-s);
+        }
+        cout<<s<<endl;
+    }
+}
