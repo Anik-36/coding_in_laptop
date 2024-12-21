d40b4af9245b852cd6b522c8deaf6ca44ba5359e
Anik-36 <delighteraranik@gmail.com>
2024-12-21 21:10:23 +0600

    it gives me runtime error

--- J_Count_Letters.cpp
+++ J_Count_Letters.cpp
@@ -8,10 +8,10 @@ int main(){
     for(int i=0 ; i<strlen(a); i++){
         f[a[i]-'a']++;
     }
-    for(int i = 0 ; i<strlen(a); i++){
-        if(f[a[i]-'a']>0){
-            printf("%c : %d\n",a[i],f[a[i]-'a']);
-            f[a[i]-'a'] = 0;
+    for(int i = 0 ; i<26; i++){
+        if(f[i]>0){
+            printf("%c : %d\n",i+'a',f[i]);
+            f[i] = 0;
         }
     }
     return 0;
