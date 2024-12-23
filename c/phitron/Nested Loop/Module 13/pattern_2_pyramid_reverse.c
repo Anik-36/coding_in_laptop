bce2055f93c678e2393552b461b2914851120c53
Anik-36 <delighteraranik@gmail.com>
2024-12-23 14:35:43 +0600

    make a reverse pyramid

--- c/phitron/Nested Loop/Module 13/pattern_2_pyramid_reverse.c
+++ c/phitron/Nested Loop/Module 13/pattern_2_pyramid_reverse.c
@@ -2,8 +2,18 @@
 int main(){
     int n;
     scanf("%d",&n);
+    int k = n*2-1;
+    int s = 0;
     for(int i = 0 ; i<n; i++){
-        
+        for(int j=1 ; j<=s; j++){
+            printf(" ");
+        }
+        for(int j=1 ; j<=k; j++){
+            printf("*");
+        }
+        printf("\n");
+        s++;
+        k-=2;
     }
     return 0;
 }
