20dad576c0310bb17dd76f9fcdf5905861d13db8
Anik-36 <delighteraranik@gmail.com>
2024-12-23 14:32:06 +0600

    make a little change in pyramic pattern for undersanding better

--- c/phitron/Nested Loop/Module 13/pattern_2_pyramic.c
+++ c/phitron/Nested Loop/Module 13/pattern_2_pyramic.c
@@ -5,10 +5,10 @@ int main(){
     int s = n - 1;
     int k = 1;
     for(int i = 0 ; i<n; i++){
-        for(int j =0; j<s; j++){
+        for(int j =1; j<=s; j++){
             printf(" ");
         }
-        for(int j =0; j<k; j++){
+        for(int j =1; j<=k; j++){
             printf("*");
         }
         printf("\n");
