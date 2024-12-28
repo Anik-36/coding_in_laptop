f5a35fd47953e52a4b8b2cd93a7867c0105f296d
Anik-36 <delighteraranik@gmail.com>
2024-12-28 21:41:48 +0600

    selection sort in ascending order

--- c/phitron/Nested Loop/Module 13/selection_sort.c
+++ c/phitron/Nested Loop/Module 13/selection_sort.c
@@ -6,28 +6,30 @@ int main(){
     for(int i = 0 ;i<n; i++){
         scanf("%d",&a[i]);
     }
-    // ascending order
-    // for(int i = 0 ; i<n-1; i++){
-    //     for(int j = i+1; j<n; j++){
-    //         if(a[i]>a[j])            {
-    //             int tmp = a[i];
-    //             a[i] = a[j];
-    //             a[j] = tmp;
-    //         }
-    //     }
-    //}
 
-    // descending order
-
+    //ascending order
+    
     for(int i = 0 ; i<n-1; i++){
         for(int j = i+1; j<n; j++){
-            if(a[i]<a[j])            {
+            if(a[i]>a[j])            {
                 int tmp = a[i];
                 a[i] = a[j];
                 a[j] = tmp;
             }
         }
     }
+
+    // descending order
+
+    // for(int i = 0 ; i<n-1; i++){
+    //     for(int j = i+1; j<n; j++){
+    //         if(a[i]<a[j])            {
+    //             int tmp = a[i];
+    //             a[i] = a[j];
+    //             a[j] = tmp;
+    //         }
+    //     }
+    // }
 
     for(int i = 0 ; i<n; i++){
         printf("%d ",a[i]);
