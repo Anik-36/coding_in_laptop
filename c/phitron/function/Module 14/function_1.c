60a59acace451195b490e96ab59661e548f026f4
Anik-36 <delighteraranik@gmail.com>
2024-12-29 20:15:47 +0600

    make a function to get a sum of two variable

--- c/phitron/function/Module 14/function_1.c
+++ c/phitron/function/Module 14/function_1.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
-return_type function_name(parameter){
-    code;
-    return what?
+
+// return_type function_name(parameter){
+//     code;
+//     return what?
+// }
+int sum(int x , int y){
+    int sum = x+y;
+    return sum;
 }
 int main(){
-
+    int s = sum(10,20);
+    printf("%d\n",s);
     return 0;
 }
