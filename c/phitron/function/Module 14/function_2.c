9c6c552372896e1d961df4b855941370db4a0488
Anik-36 <delighteraranik@gmail.com>
2024-12-30 13:57:01 +0600

    without paratmeter reuturn somthing

---
+++ c/phitron/function/Module 14/function_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int sum(){
+    int x, y;
+    scanf("%d%d",&x,&y);
+    int s = x+y;
+    return s;
+}
+int main(){
+    int s =  sum();
+    printf("%d\n",s);
+    return 0;
+}
