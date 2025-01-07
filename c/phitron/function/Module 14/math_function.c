8c85256e12c35db8f2c7812cf78376ad82f3ed9f
Anik-36 <delighteraranik@gmail.com>
2025-01-08 00:20:29 +0600

    sqrt function is worked

--- c/phitron/function/Module 14/math_function.c
+++ c/phitron/function/Module 14/math_function.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    double a;
-    scanf("%lf",&a);
-    //double ans = ceil(a);
-    //double ans = floor(a);
-    double ans  = round(a);
-    printf("%lf\n",ans);
+    // double a;
+    // scanf("%lf",&a);
+    // //double ans = ceil(a);
+    // //double ans = floor(a);
+    // double ans  = round(a);
+    // printf("%lf\n",ans);
 
+    double b;
+    scanf("%lf",&b);
+    double ans = sqrt(b);
+    printf("%lf\n",ans);
 
     return 0;
 }
