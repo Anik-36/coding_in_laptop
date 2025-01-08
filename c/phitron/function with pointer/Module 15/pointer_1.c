a5e843e7acfe15d6a8c6b74970fb0057abcd80de
Anik-36 <delighteraranik@gmail.com>
2025-01-08 19:48:23 +0600

    just play with the pointer

--- c/phitron/function with pointer/Module 15/pointer_1.c
+++ c/phitron/function with pointer/Module 15/pointer_1.c
@@ -3,12 +3,27 @@ int main(){
     int a = 50;
     // the datatype of the pointer always should be the variable datatype. because pointer has no datatype
     int * p = &a;
-    // derefferencing
+    // dereferencing
     *p = 200;
+    int **p2 = &p; // double referencing
+    // printing a's address
     printf("a er address -> %p\n",&a);
     printf("a er address -> %p\n",p);
+    // printing a's value
     printf("a er value -> %d\n",a);
     printf("a er value -> %d\n",*p);
+    //printing p's address
+    printf("p er address -> %p\n",&p);
+    printf("p er address -> %p\n",p2);
+    // printing p's value
+    printf("a er value -> %d\n",**p2);
+    printf("a er address -> %p\n",*p2);
+    int *** p3 = &p2; // triple refferencing
+    printf("a er value -> %d\n",***p3);
+    printf("p er address -> %p\n",*p3);
+    printf("p2 er address -> %p\n",p3);
+    printf("p2 er address -> %p\n",&p2);
+
     printf("pointer er size -> %d\n",sizeof(p));
     return 0;
 }
