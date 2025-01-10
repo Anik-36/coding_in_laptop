7f9d81483963b5a6f610bf7c05239b2d86260f2c
Anik-36 <delighteraranik@gmail.com>
2025-01-10 18:26:42 +0600

    add the question

--- c/phitron/function with pointer/Module 15.5/Practice_probelm/Problem_1.c
+++ c/phitron/function with pointer/Module 15.5/Practice_probelm/Problem_1.c
@@ -1,3 +1,4 @@
+//Question: Write a function named my_abs() that receives an integer value as parameter and returns the absolute value of that. Absolute value means if the value is negative it will be converted to positive value. Then print the value in the main function.
 #include<stdio.h>
 #include<stdlib.h>
 int my_abs(int a){
