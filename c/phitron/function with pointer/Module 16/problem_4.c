8f37a37a3a6efef27449408324502f0b0f657068
Anik-36 <delighteraranik@gmail.com>
2025-01-12 12:31:59 +0600

    make 4 function to practice

---
+++ c/phitron/function with pointer/Module 16/problem_4.c
@@ -0,0 +1,23 @@
+//Question: Show the 4 types of examples of functions given below with an example. You can give any example you want, but make sure you are giving different examples for all the four types..					Marks: 20
+// Has Return + Parameter
+// Has Return + No Parameter
+// No Return + Parameter
+// No Return + No Parameter
+
+#include<stdio.h>
+int return_parameter(int a, int b){
+
+}
+int return_no_parameter(){
+
+}
+void return_parameter(int a){
+
+}
+void no_return_no_parameter(){
+
+}
+int main(){
+
+    return 0;
+}
