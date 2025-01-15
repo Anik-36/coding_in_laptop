cce81c1f9110d652fb6ab1b8257518253fb4470a
Anik-36 <delighteraranik@gmail.com>
2025-01-16 00:02:03 +0600

    finally find the bug

--- c/phitron/Module 18-2D Array/input_output.c
+++ c/phitron/Module 18-2D Array/input_output.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main(){
     int a[4][3];
-    for(int i =0; i<4; i++){
+    for(int i = 0; i<4; i++){
         for(int j = 0; j<3; j++){
-            scanf("%d%d",&a[i],&a[j]);
+            scanf("%d",&a[i][j]);
         }
-        printf("\n");
     }
     for(int i =0; i<4; i++){
         for(int j = 0; j<3; j++){
-            printf("%d%d ",a[i],a[j]);
+            printf("%d ",a[i][j]);
         }
         printf("\n");
     }
