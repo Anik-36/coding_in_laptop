f299fd04c18d8eea08488082061efac1e608f861
Anik-36 <delighteraranik@gmail.com>
2025-01-15 23:49:13 +0600

    now it looks like matrix

--- c/phitron/Module 18-2D Array/input_output.c
+++ c/phitron/Module 18-2D Array/input_output.c
@@ -3,7 +3,7 @@ int main(){
     int a[4][3];
     for(int i =0; i<4; i++){
         for(int j = 0; j<3; j++){
-            printf("a[%d][%d]",i,j);
+            printf("a[%d][%d] ",i,j);
         }
         printf("\n");
     }
