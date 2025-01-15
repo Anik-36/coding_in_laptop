96240699156d2436be62236f004a0c218db72bf0
Anik-36 <delighteraranik@gmail.com>
2025-01-15 23:56:50 +0600

    something else

--- c/phitron/Module 18-2D Array/input_output.c
+++ c/phitron/Module 18-2D Array/input_output.c
@@ -3,7 +3,13 @@ int main(){
     int a[4][3];
     for(int i =0; i<4; i++){
         for(int j = 0; j<3; j++){
-            printf("a[%d][%d] ",i,j);
+            scanf("%d%d",&a[i],&a[j]);
+        }
+        printf("\n");
+    }
+    for(int i =0; i<4; i++){
+        for(int j = 0; j<3; j++){
+            printf("%d%d ",a[i],a[j]);
         }
         printf("\n");
     }
