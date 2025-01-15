29007d89ab989b5b5d71987bd0634ed9eb8b4904
Anik-36 <delighteraranik@gmail.com>
2025-01-15 23:48:52 +0600

    making it real matrix

--- c/phitron/Module 18-2D Array/input_output.c
+++ c/phitron/Module 18-2D Array/input_output.c
@@ -3,8 +3,9 @@ int main(){
     int a[4][3];
     for(int i =0; i<4; i++){
         for(int j = 0; j<3; j++){
-            printf("a[%d] a[%d]\n",i,j);
+            printf("a[%d][%d]",i,j);
         }
+        printf("\n");
     }
     return 0;
 }
