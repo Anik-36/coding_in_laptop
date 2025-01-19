2db185bd2594f11a99f12bdf3d056fbb855f7174
Anik-36 <delighteraranik@gmail.com>
2025-01-19 18:09:52 +0600

    finally it is solved

--- c/phitron/Module 19-Recursion Recape/test_2D_array_in_recursion.c
+++ c/phitron/Module 19-Recursion Recape/test_2D_array_in_recursion.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void print_array(int a[r][c], int r,int c){
+void print_array(int r,int c,int a[r+1][c+1] ){
     // if(r<0 && c<0) return;
     // print_array(a,r-1,c-1);
     // printf("%d ",a[r][c]);
@@ -20,6 +20,32 @@ int main(){
             scanf("%d",&a[i][j]);
         }
     }
-    print_array(a,r-1,c-1);
+    print_array(r-1,c-1,a);
     return 0;
 }
+
+
+
+
+
+// // The following program works only if your compiler is C99 compatible.
+// #include <stdio.h>
+
+// // n must be passed before the 2D array
+// void print(int m, int n, int arr[][n]){
+//     int i, j;
+//     for (i = 0; i < m; i++){
+//         for (j = 0; j < n; j++){
+//             printf("%d ", arr[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int main()
+// {
+//     int arr[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//     int m = 3, n = 3;
+//     print(m, n, arr);
+//     return 0;
+// }
