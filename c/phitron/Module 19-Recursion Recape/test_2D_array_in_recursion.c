abd535bc9479ae2b1d85de92e85d67f91b2f2e4f
Anik-36 <delighteraranik@gmail.com>
2025-01-19 18:11:45 +0600

    trying to solve with recursion

--- c/phitron/Module 19-Recursion Recape/test_2D_array_in_recursion.c
+++ c/phitron/Module 19-Recursion Recape/test_2D_array_in_recursion.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void print_array(int r,int c,int a[r+1][c+1] ){
-    // if(r<0 && c<0) return;
-    // print_array(a,r-1,c-1);
-    // printf("%d ",a[r][c]);
-    for(int i = 0; i<=r; i++){
-        for(int j = 0; j<=c; j++){
-            printf("%d ",a[i][j]);
-        }
-        printf("\n");
-    }
+    if(r<0 && c<0) return;
+    print_array(r-1,c-1,a);
+    printf("%d ",a[r][c]);
+    // for(int i = 0; i<=r; i++){
+    //     for(int j = 0; j<=c; j++){
+    //         printf("%d ",a[i][j]);
+    //     }
+    //     printf("\n");
+    // }
     return;
 }
 int main(){
