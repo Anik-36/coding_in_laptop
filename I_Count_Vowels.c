ebb14353e269b57d1a52cef11cfed4f22403db27
Anik-36 <delighteraranik@gmail.com>
2025-01-20 21:01:18 +0600

    the count_vowel problem solved

--- I_Count_Vowels.c
+++ I_Count_Vowels.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int count_vowel(char * s,int i){
-    if(i=='\0') return 0;
+    if(s[i]=='\0') return 0;
     int ans = count_vowel(s,i+1);
     if(s[i]>='A' && s[i]<='Z'){
         s[i] = s[i] + 32;
@@ -12,8 +12,8 @@ int count_vowel(char * s,int i){
     }
 }
 int main(){
-    char s[206];
-    fgets(s,206,stdin);
+    char s[205];
+    fgets(s,205,stdin);
     int count = count_vowel(s,0);
     printf("%d\n",count);
     return 0;
