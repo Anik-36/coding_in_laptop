08680073421af4a512d6629c34689a2c85878c0d
Anik-36 <delighteraranik@gmail.com>
2025-01-22 01:20:33 +0600

    ternary operator

---
+++ c/phitron/C++/Module 2-Dynamic Memory Allocation/ternary.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int a = 11;
+    // if(a%2==0) cout<<"even"<<endl;
+    // else cout<<false<<endl;
+    (a%2==0)?cout<<"even"<<endl:cout<<"odd"<<endl;
+}
