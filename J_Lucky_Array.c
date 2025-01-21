34dd616affe74f5acaa747ae87f315303d5cb1ef
Anik-36 <delighteraranik@gmail.com>
2025-01-21 17:35:16 +0600

    correct code is here

--- J_Lucky_Array.c
+++ J_Lucky_Array.c
@@ -4,14 +4,16 @@ int main(){
     int n;
     scanf("%d",&n);
     int a[n];
-    int f[10010] = {0};
     int mn = INT_MAX;
     for(int i = 0; i<n; i++){
         scanf("%d",&a[i]);
-        f[a[i]]++;
         if(a[i]<mn) mn = a[i];
     }
-    if(f[mn]%2!=0) printf("Lucky\n");
+    int ct = 0;
+    for(int i = 0;  i<n; i++){
+        if(a[i]==mn) ct++;
+    }
+    if(ct%2!=0) printf("Lucky\n");
     else printf("Unlucky\n");
     return 0;
 }
