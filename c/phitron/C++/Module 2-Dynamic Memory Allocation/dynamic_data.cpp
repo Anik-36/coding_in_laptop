8d4cd1184fb295774d3e89ebc2eba74bb47182c6
Anik-36 <delighteraranik@gmail.com>
2025-01-22 00:34:31 +0600

    making gynamic address

---
+++ c/phitron/C++/Module 2-Dynamic Memory Allocation/dynamic_data.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int * a = new int ;
+    * a = 40;
+    cout<<a<<endl;
+    cout<<*a<<endl;
+}
