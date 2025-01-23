13a59ecdc02da252ddbe824fa6f9052ea525eee1
Anik-36 <delighteraranik@gmail.com>
2025-01-23 14:37:11 +0600

    okay. sizeof() give the byte value of the datatype based on the required machine

--- c/phitron/C++/Week 2/Module 5-String in c++/string_capacity_function.cpp
+++ c/phitron/C++/Week 2/Module 5-String in c++/string_capacity_function.cpp
@@ -3,21 +3,24 @@ using namespace std;
 int main(){
     // string s = "Hello";
     // cout<<s.size()<<endl;
-    // cout<<s.max_size()<<endl; // 10^5
+    // // cout<<s.max_size()<<endl; // 10^5
 
-    // string s ="hello_my_name_is_anik";
-    // cout<<s.capacity()<<endl; // 21 // it will increase size dynamically into highest array size
-    // s = "hello_yes_my_name_is_anik_but_do_u_increase_your_size";
-    // cout<<s.capacity()<<endl; // 53
-    string s = "hello_yes_my_name_is_anik_but_do_u_increase_your_size1";
-    cout<<s.capacity()<<endl; // 106
-    //s.clear(); // it will clear the string
-    // cout<<s<<endl; // the string is empty
-    // if(s.empty()==true) cout<<"yes"<<endl;
-    // else cout<<"no"<<endl;
-    // s.resize(10);
-    cout<<s<<endl;
-    // cout<<s.size()<<endl;
-    cout<<sizeof(s)<<endl;
+    // // string s ="hello_my_name_is_anik";
+    // // cout<<s.capacity()<<endl; // 21 // it will increase size dynamically into highest array size
+    // // s = "hello_yes_my_name_is_anik_but_do_u_increase_your_size";
+    // // cout<<s.capacity()<<endl; // 53
+    // string s = "hello_yes_my_name_is_anik_but_do_u_increase_your_size1";
+    // cout<<s.capacity()<<endl; // 106
+    // //s.clear(); // it will clear the string
+    // // cout<<s<<endl; // the string is empty
+    // // if(s.empty()==true) cout<<"yes"<<endl;
+    // // else cout<<"no"<<endl;
+    // // s.resize(10);
+    // cout<<s<<endl;
+    // // cout<<s.size()<<endl;
+    // cout<<sizeof(s)<<endl;
+
+    string a;
+    cout<<sizeof(a)<<endl;
 
 }
