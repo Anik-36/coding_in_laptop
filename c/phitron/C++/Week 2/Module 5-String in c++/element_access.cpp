0f277e50a5e21a963235e57cb09463ac0dd22ab5
Anik-36 <delighteraranik@gmail.com>
2025-01-23 14:43:45 +0600

    elemment acccess is done

---
+++ c/phitron/C++/Week 2/Module 5-String in c++/element_access.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    cout<<s[0]<<endl;
+    cout<<s.at(0)<<endl;
+    cout<<s.front()<<endl;
+    cout<<s[s.size()-1]<<endl;
+    cout<<s.back()<<endl;
+}
