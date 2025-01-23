c62b07e3ae25be4a39336f6f5d7ca07222c9b836
Anik-36 <delighteraranik@gmail.com>
2025-01-23 19:47:21 +0600

    string newline problem solved

---
+++ c/phitron/C++/Week 2/Module 5-String in c++/string_input_space.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    cout<<s<<endl;
+}
