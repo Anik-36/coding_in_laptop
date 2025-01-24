744de50359abc2e46730d58801d450abd38f25f4
Anik-36 <delighteraranik@gmail.com>
2025-01-24 11:20:42 +0600

    learnt sort function

---
+++ c/phitron/C++/Week 2-DSA/Module 6-more about class and string/sort_string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    string s;
+    // cin>>s;
+    getline(cin,s);
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+}
