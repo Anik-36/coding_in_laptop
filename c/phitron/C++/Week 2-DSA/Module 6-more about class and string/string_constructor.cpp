ea06f6db70ef4928fd532257ca48afb37ec7d1e4
Anik-36 <delighteraranik@gmail.com>
2025-01-24 11:15:39 +0600

    various use of string object

---
+++ c/phitron/C++/Week 2-DSA/Module 6-more about class and string/string_constructor.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+u`sing namespace std;
+int main(){
+    // string s = "hello world";
+
+    // rules 1
+    // string s("hello world 1st"); // hello world
+
+    // rule 2
+    // string s("hello world",4); // hell
+
+    // rule 3
+    // string a = "hello world";
+
+    // rule 4
+    //string s(a,4); // o world
+
+    // rule 5
+    string s(5,'x');xxxxx
+    
+    cout<<s<<endl;
+}
