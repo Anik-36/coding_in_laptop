b99433abd06d7f23893236ea9c41fd82a36b60c6
Anik-36 <delighteraranik@gmail.com>
2025-01-24 11:46:24 +0600

    reverse printing is done

--- c/phitron/C++/Week 2-DSA/Module 6-more about class and string/reverse_word_printing.cpp
+++ c/phitron/C++/Week 2-DSA/Module 6-more about class and string/reverse_word_printing.cpp
@@ -4,13 +4,17 @@
 #define yes cout<<"YES"<<endl;
 #define no cout<<"NO"<<endl;
 using namespace std;
+void print(stringstream & ss){
+    string word;
+    if(ss>>word){
+        print(ss);
+        cout<<word<<endl;
+    }
+}
 int main(){
     string s;
     getline(cin,s);
-    stringstream ss;
-    ss<<s;
-    string word;
-    while(ss>>word){
-        cout<<word<<endl;
-    }
+    stringstream ss(s);
+    // ss<<s;
+    print(ss);
 }
