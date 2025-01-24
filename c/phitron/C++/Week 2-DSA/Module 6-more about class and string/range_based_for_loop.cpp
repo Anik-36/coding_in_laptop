d68afbf79b6d25a33f789b11f68df1917ebc6902
Anik-36 <delighteraranik@gmail.com>
2025-01-24 11:27:31 +0600

    shortcut for loop learnt

---
+++ c/phitron/C++/Week 2-DSA/Module 6-more about class and string/range_based_for_loop.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    string s;
+    // cin>>s;
+    getline(cin,s);
+    // for(int i = 0; i<s.size(); i++){
+    //     cout<<s[i]<<endl;
+    // }
+    for(char c:s){
+        cout<<c<<endl;
+    }
+}
