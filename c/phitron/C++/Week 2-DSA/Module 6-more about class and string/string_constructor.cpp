693559a3c2618dfbe5f0e282da2134242756beae
Anik-36 <delighteraranik@gmail.com>
2025-01-25 20:20:34 +0600

    little bit change

--- c/phitron/C++/Week 2-DSA/Module 6-more about class and string/string_constructor.cpp
+++ c/phitron/C++/Week 2-DSA/Module 6-more about class and string/string_constructor.cpp
@@ -1,22 +1,20 @@
 #include<bits/stdc++.h>
-u`sing namespace std;
+using namespace std;
 int main(){
     // string s = "hello world";
 
     // rules 1
-    // string s("hello world 1st"); // hello world
+    string s("hello world 1st"); // hello world
 
     // rule 2
     // string s("hello world",4); // hell
 
     // rule 3
     // string a = "hello world";
-
-    // rule 4
     //string s(a,4); // o world
 
-    // rule 5
-    string s(5,'x');xxxxx
-    
+    // rule 4
+    // string s(5,'x'); // xxxxx
+
     cout<<s<<endl;
 }
