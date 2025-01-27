6ae541d29fee243bbcd653ce4f33015049d59365
Anik-36 <delighteraranik@gmail.com>
2025-01-27 20:48:55 +0600

    there a problem has ditected.

---
+++ c/phitron/C++/Week 2-DSA/Module 7-Array of Object/array_of_objects.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+class Student{
+    public:
+        string name;
+        int roll;
+        int marks;
+
+        // Student(string name,int roll,int marks){
+        //     this->name  = name;
+        //     this->roll  = roll;
+        //     this->marks = marks;
+        // }
+        // it is not possible to give input for object of array by constructor. so we don't use constructor.
+
+};
+int main(){
+    Student a[3];
+    for(int i = 0; i<3; i++){
+        getline(cin,a[i].name);
+        cin>>a[i].roll>>a[i].marks;
+    }
+    for(int i = 0; i<3; i++){
+        cout<<a[i].name<<" "<<a[i].roll<<" "<<a[i].marks<<endl;
+    }
+}
