5b5d887ce87bfcba475b94b7f810970c45cbf0f1
Anik-36 <delighteraranik@gmail.com>
2025-01-27 21:23:27 +0600

    max min object done

---
+++ c/phitron/C++/Week 2-DSA/Module 7-Array of Object/max_min_from_object.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+class Student{
+    public:
+        string name;
+        int roll;
+        int marks;
+};
+int main(){
+    Student a[3];
+    for(int i = 0; i<3; i++){
+        getline(cin,a[i].name);
+        cin>>a[i].roll>>a[i].marks;
+        cin.ignore();
+    }
+    Student mx;
+    mx.marks = INT_MIN;
+    for(int i = 0; i<3; i++){
+        if(a[i].marks>mx.marks){
+            mx = a[i];
+        }
+    }
+    cout<<mx.name<<" "<<mx.roll<<" "<<mx.marks<<endl;
+
+    Student mn;
+    mn.marks = INT_MAX;
+    for(int i = 0; i<3; i++){
+        if(a[i].marks<mn.marks){
+            mn = a[i];
+        }
+    }
+    cout<<mn.name<<" "<<mn.roll<<" "<<mn.marks<<endl;
+}
