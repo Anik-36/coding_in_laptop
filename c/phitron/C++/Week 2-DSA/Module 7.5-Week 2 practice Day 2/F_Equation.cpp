06113d83a930efb8827792f3397a6b827b242394
Anik-36 <delighteraranik@gmail.com>
2025-02-02 18:54:48 +0600

    not solve f_equation

---
+++ c/phitron/C++/Week 2-DSA/Module 7.5-Week 2 practice Day 2/F_Equation.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long equation(long long x,long long n,long long sum){
+    // if(n==0) return 0;
+    // // cout<<x<<" "<<n<<" "<<" ";
+    // sum = equation(x,n-2,sum);
+    // // cout<<sum<<endl;
+    // cout<<pow(x,n)<<endl;
+    // sum= sum + pow(x,n);
+    for(int i = 2 ; i<=n; i+=2){
+        sum = sum + pow(x,i);
+        cout<<sum<<endl;
+    }
+    return sum;
+}
+int main(){
+    long long x,n;
+    cin>>x>>n;
+    long long sum = 0;
+    if(n%2==0){
+        // sum = equation(x,n,0);
+        for(int i = 2; i<=n; i+=2){
+            sum+=pow(x,i);
+        }
+    }else{
+        // sum = equation(x,n-1,0);
+        for(int i = 2; i<n; i+=2){
+            sum+=pow(x,i);
+        }
+    }
+    cout<<sum<<endl;
+}
