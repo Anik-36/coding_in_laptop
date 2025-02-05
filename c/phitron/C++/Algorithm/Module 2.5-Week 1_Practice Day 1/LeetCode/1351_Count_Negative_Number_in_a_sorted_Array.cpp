61e6efd89a95be7092b34ec8efa5c482ee0ef0db
Anik-36 <delighteraranik@gmail.com>
2025-02-05 14:49:05 +0600

    trying to solve efficiently

--- c/phitron/C++/Algorithm/Module 2.5-Week 1_Practice Day 1/LeetCode/1351_Count_Negative_Number_in_a_sorted_Array.cpp
+++ c/phitron/C++/Algorithm/Module 2.5-Week 1_Practice Day 1/LeetCode/1351_Count_Negative_Number_in_a_sorted_Array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        // int ct = 0;
-        // int ans = 0;
-        // for(int i = 0; i<grid.size(); i++){
-        //     int l = 0;
-        //     int u = grid[0].size();
-        //     while(l<u){
-        //         int m = (l+u)/2;
-        //         if(grid[i][m]>=0){
-        //             l = m+1;
-        //         }else{
-        //             u = m;
-        //         }
-        //         ans = m;
-        //     }
-        //     ct += grid[0].size()-ans;
-        // }
+        int ct = 0;
+        int ans = 0;
         for(int i = 0; i<grid.size(); i++){
-            for(int j = 0; j<grid[0].size(); j++){
-                if(grid[i][j]<0) ct++;
+            int l = 0;
+            int u = grid[0].size();
+            while(l<u){
+                int m = (l+u)/2;
+                if(grid[i][m]>=0){
+                    l = m+1;
+                }else{
+                    u = m;
+                }
+                ans = m;
             }
+            ct += grid[0].size()-ans;
         }
+        // for(int i = 0; i<grid.size(); i++){
+        //     for(int j = 0; j<grid[0].size(); j++){
+        //         if(grid[i][j]<0) ct++;
+        //     }
+        // }
         return ct;
     }
 };
