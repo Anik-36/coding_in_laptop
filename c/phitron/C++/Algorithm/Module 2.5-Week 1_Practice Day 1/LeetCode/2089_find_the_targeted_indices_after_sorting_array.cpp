1c3e18d2a869b4a11e26d6db3c4da9656ecdb54e
Anik-36 <delighteraranik@gmail.com>
2025-02-07 12:13:56 +0600

    trying

--- c/phitron/C++/Algorithm/Module 2.5-Week 1_Practice Day 1/LeetCode/2089_find_the_targeted_indices_after_sorting_array.cpp
+++ c/phitron/C++/Algorithm/Module 2.5-Week 1_Practice Day 1/LeetCode/2089_find_the_targeted_indices_after_sorting_array.cpp
@@ -5,39 +5,69 @@
 #define no cout<<"NO"<<endl;
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i = 0; i<n; i++){
-        cin>>a[i];
-    }
-    int ans ;
-    int target;
-    cin>>target;
+    int a[]= {1,2,5,2,3};
+    int n = sizeof(a)/sizeof(int);
+    int ans;
+    int target = 2;
     sort(a,a+n);
     for(int i = 0; i<n; i++){
         cout<<a[i]<<" ";
     }
-    int b[]={};
-    int l = 0;
-    int u = n-1;
-    while(l<u){
-        int m = (l+u)/2;
-        if(a[m]>target){
-            u = m;
-        }else if(a[m]<target){
-            l = m+1;
-        }else{
-            l = m;
-        }
-        ans = l;
-    }
-    if(a[l]==target){
-        for(int i = 0, j = l; a[j]==target; j++,i++){
-            b[i] = j;
-        }
-    }
-    for(int i = 0; i<sizeof(b)/sizeof(b[0]); i++){
-        cout<<b[i]<<" ";
-    }
+    // int b[n]={};
+    // int l = 0;
+    // int u = n-1;
+    // while(l<u){
+    //     int m = (l+u)/2;
+    //     if(a[m]>target){
+    //         u = m;
+    //     }else if(a[m]<target){
+    //         l = m+1;
+    //     }else{
+    //         l = m;
+    //     }
+    //     ans = l;
+    // }
+    // if(a[l]==target){
+    //     for(int i = 0, j = l; a[j]==target; j++,i++){
+    //         b[i] = j;
+    //     }
+    // }
+    // for(int i = 0; i<sizeof(b)/sizeof(b[0]); i++){
+    //     cout<<b[i]<<" ";
+    // }
 }
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& a, int target) {
+        int n = a.size();
+        sort(a.begin(),a.end());
+        vector<int> b;
+        int ans;
+        int l = 0;
+        int u = n-1;
+        while(l<u){
+            int mid = (l+u)/2;
+            if(a[mid]<target){
+                l = mid + 1;
+            }else if(a[mid]>target){
+                u = mid;
+            }else{
+                l = mid;
+            }
+            ans = l;
+        }
+        if(a[l]==target){
+            for(int i = l; a[i]==target; i++){
+                b.pushback(i);
+            }
+        }
+        return b;
+    }
+};
