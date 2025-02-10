d76f8db3f1a30c21ba80327319ffe861b6bc0917
Anik-36 <delighteraranik@gmail.com>
2025-02-10 14:33:59 +0600

    O(logN) complexity

---
+++ c/phitron/C++/Basic Data Structure/O_log_N.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n>0){
+        int r = n%10;
+        cout<<r<<endl;
+        n/=10;
+    }
+}
