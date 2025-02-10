f48856f4de61de2b6e1ea79e66e4b95c0176a1c6
Anik-36 <delighteraranik@gmail.com>
2025-02-10 17:56:51 +0600

    just simple change of the code

--- c/phitron/C++/Basic Data Structure/Module 1-Time Complexity/O_sqrtN.cpp
+++ c/phitron/C++/Basic Data Structure/Module 1-Time Complexity/O_sqrtN.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    // for(int i = 1; i<=sqrt(n); i++){  // O(logN)
+    // for(int i = 1; i<=sqrt(n); i++){  // O(sqrt(N))
     //     if(n%i==0){
     //         cout<<i<<" ";
     //         if(n/i!=i) cout<<n/i<<endl;
