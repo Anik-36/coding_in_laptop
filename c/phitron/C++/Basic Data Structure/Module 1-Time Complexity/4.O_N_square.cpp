252a8fc3de207e89f59122ba36b30c6b78a73a28
Anik-36 <delighteraranik@gmail.com>
2025-02-10 18:12:34 +0600

    time complexity o(n^2)

---
+++ c/phitron/C++/Basic Data Structure/Module 1-Time Complexity/4.O_N_square.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        cout<<"Hello"<<endl;
+    }
+}
