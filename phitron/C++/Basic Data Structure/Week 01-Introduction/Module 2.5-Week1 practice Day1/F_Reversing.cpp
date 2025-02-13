671bee0af64b78b6de3ab87a3d716a7e288dc7ed
Anik-36 <delighteraranik@gmail.com>
2025-02-13 15:49:24 +0600

    using reverse function to reverse a vector

--- phitron/C++/Basic Data Structure/Week 01-Introduction/Module 2.5-Week1 practice Day1/F_Reversing.cpp
+++ phitron/C++/Basic Data Structure/Week 01-Introduction/Module 2.5-Week1 practice Day1/F_Reversing.cpp
@@ -11,7 +11,8 @@ int main(){
     for(int i = 0; i<n; i++){
         cin>>v[i];
     }
-    for(int i = v.size()-1; i>=0; i--){
+    reverse(v.begin(),v.end());
+    for(int i = 0; i<v.size(); i++){
         cout<<v[i]<<" ";
     }
 }
