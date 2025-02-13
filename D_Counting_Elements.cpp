c2ee3e4a956d42e009a9725fb576d70c006f47f9
Anik-36 <delighteraranik@gmail.com>
2025-02-13 12:04:31 +0600

    counting element is solved

--- D_Counting_Elements.cpp
+++ D_Counting_Elements.cpp
@@ -7,12 +7,18 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int> a[n];
+    vector<int> a(n);
+    vector<int> fr(10e3+10,0);
+    int ct = 0;
     for(int i = 0; i<n; i++){
         cin>>a[i];
+        fr[a[i]]++;
     }
     sort(a.begin(),a.end());
     for(int i = 0 ; i<n; i++){
-        cout<<a[i]<<" ";
+        if(fr[a[i]+1]>0){
+            ct++;
+        }
     }
+    cout<<ct<<endl;
 }
