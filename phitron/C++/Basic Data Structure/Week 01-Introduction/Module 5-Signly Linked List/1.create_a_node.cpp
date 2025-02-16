913ceebc257511ef9017bf99523b5bb03b6aa133
Anik-36 <delighteraranik@gmail.com>
2025-02-16 23:32:35 +0600

    how to created a node for linked list

---
+++ phitron/C++/Basic Data Structure/Week 01-Introduction/Module 5-Signly Linked List/1.create_a_node.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* next;
+};
+int main(){
+    Node a,b;
+    a.val = 10;
+    b.val = 20;
+    a.next = &b;
+    cout<<a.val<<endl;
+    cout<<b.val<<endl;
+    cout<<(*a.next).val<<endl;
+}
