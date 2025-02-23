670656117a13150e8cd963491f4f95661b0125cd
Anik-36 <delighteraranik@gmail.com>
2025-02-24 00:01:52 +0600

    the linked list is sorted

---
+++ phitron/C++/Basic Data Structure/Week 02 - Linked List/Module 7-Singly Linked List Recap/8.sort_linked_list.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+        }
+};
+void insert_at_tail(Node * &head, Node * &tail, int val){  // O(1)
+    Node * newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+void print_linked_list(Node * head){
+    Node * tmp = head;
+    while(tmp != NULL){
+        cout<<tmp->val<<" ";
+        tmp = tmp->next;
+    }
+    cout<<endl;
+}
+int main(){
+    Node * head = NULL;
+    Node * tail = NULL;
+    while(true){
+        int val;
+        cin>>val;
+        if(val == -1) break;
+        insert_at_tail(head,tail,val);
+        for(Node * i = head; i->next != NULL; i = i->next){
+            for(Node * j = i->next; j!=NULL; j = j->next){
+                if(i->val > j->val){
+                    swap(i->val , j->val);
+                }
+            }
+        }
+    }
+    print_linked_list(head);
+}
