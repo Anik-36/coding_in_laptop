b23933f7808d9b705b260eb00fbd16128f90f6be
Anik-36 <delighteraranik@gmail.com>
2025-02-27 11:00:33 +0600

    copy vlaue from another arrqay into list

--- phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/1.list_constructor.cpp
+++ phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/1.list_constructor.cpp
@@ -8,8 +8,12 @@ int main(){
     // list<int> myList;
     // list<int> myList(10);
     // list<int> myList(10,100);
-    list<int> list2 = {1,2,3,4,5};
-    list<int> myList(list2);
+
+    // list<int> list2 = {1,2,3,4,5};
+    // list<int> myList(list2);
+
+    int a[5] = {10,20,30,40,50};
+    list<int> myList(a,a+5);
 
     // cout<<myList.front()<<endl;
     // for(auto it = myList.begin(); it!=myList.end(); it++){
