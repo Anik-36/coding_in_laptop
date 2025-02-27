1345eed5bba99bee9a66175e33d57941645dbd8e
Anik-36 <delighteraranik@gmail.com>
2025-02-27 10:57:41 +0600

    using for each looop

--- phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/1.list_constructor.cpp
+++ phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/1.list_constructor.cpp
@@ -7,12 +7,15 @@ using namespace std;
 int main(){
     // list<int> myList;
     // list<int> myList(10);
-    list<int> myList(10,5);
+    list<int> myList(10,100);
 
     // cout<<myList.front()<<endl;
-    for(auto it = myList.begin(); it!=myList.end(); it++){
-        cout<<*it<<endl;
-    }
+    // for(auto it = myList.begin(); it!=myList.end(); it++){
+    //     cout<<*it<<endl;
+    // }
 
+    for(int val:myList){
+        cout<<val<<endl;
+    }
     // cout<<myList.size()<<endl;
 }
