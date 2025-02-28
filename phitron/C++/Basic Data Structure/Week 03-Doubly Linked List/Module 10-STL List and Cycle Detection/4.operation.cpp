dc2ecb3dbe522b78dd6795061b00116466a0224e
Anik-36 <delighteraranik@gmail.com>
2025-02-28 15:33:31 +0600

    list has its own reverse funcion

--- phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/4.operation.cpp
+++ phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/4.operation.cpp
@@ -5,12 +5,17 @@
 #define no cout<<"NO"<<endl;
 using namespace std;
 int main(){
-    list<int> myList = {40,28,10,30,50,10,10};
+    list<int> myList = {40,20,10,30,50,10,10};
 
     // myList.remove(10);
 
     // myList.sort();
-    myList.sort(greater<int>());
+    // myList.sort(greater<int>());
+
+    // myList.sort();
+    // myList.unique(); // must use sort function before
+
+    myList.reverse();
     for(int val:myList){
         cout<<val<<endl;
     }
