36957da1456e4e55638cb178deb12677133f3a5f
Anik-36 <delighteraranik@gmail.com>
2025-02-28 15:29:34 +0600

    sort funcion of list is done

--- phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/4.operation.cpp
+++ phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Module 10-STL List and Cycle Detection/4.operation.cpp
@@ -7,7 +7,10 @@ using namespace std;
 int main(){
     list<int> myList = {40,28,10,30,50,10,10};
 
-    myList.remove(10);
+    // myList.remove(10);
+
+    // myList.sort();
+    myList.sort(greater<int>());
     for(int val:myList){
         cout<<val<<endl;
     }
