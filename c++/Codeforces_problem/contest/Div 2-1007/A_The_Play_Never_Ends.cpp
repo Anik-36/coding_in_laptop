d3781cceecbd5881683e6e6bd308eba910dcfc4f
Anik-36 <delighteraranik@gmail.com>
2025-02-28 21:06:41 +0600

    div - 2 round 1007 problem A solved

---
+++ c++/Codeforces_problem/contest/Div 2-1007/A_The_Play_Never_Ends.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll k;
+        cin>>k;
+        if(k%3 == 1){
+            yes
+        }else no
+    }
+}
