d4642180501423cbf120826e9075619347f25e49
Anik-36 <delighteraranik@gmail.com>
2025-03-03 22:48:38 +0600

    using slow and fast method to find the middle element of linked list

---
+++ phitron/C++/Basic Data Structure/Week 03-Doubly Linked List/Moduel 11-Problem Solving using Linked List/11.1_middle_of_linked_list_2.cpp
@@ -0,0 +1,16 @@
+/*
+slow and fast method
+*/
+
+class Solution {
+    public:
+        ListNode* middleNode(ListNode* head) {
+            ListNode * slow = head;
+            ListNode * fast = head;
+            while(fast != NULL and fast->next != NULL){
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            return slow;
+        }
+    };
