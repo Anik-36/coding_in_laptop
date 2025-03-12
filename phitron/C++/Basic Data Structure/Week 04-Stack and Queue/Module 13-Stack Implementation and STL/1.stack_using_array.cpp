86b254323a1f2a7f8018d24996afac03bfbebf21
Anik-36 <delighteraranik@gmail.com>
2025-03-12 11:31:36 +0600

    make some operation of stack

---
+++ phitron/C++/Basic Data Structure/Week 04-Stack and Queue/Module 13-Stack Implementation and STL/1.stack_using_array.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+class myStack{
+    public:
+    vector<int> v;
+    void push(int val){
+        v.push_back(val);
+    }
+    void pop(){
+        v.pop_back();
+    }
+    int top(){
+        return v.back();
+    }
+    int size(){
+        return v.size();
+    }
+    bool empty(){
+        if(v.size == 0) return true;
+        else return false;
+    }
+};
+int main(){
+    myStack st;
+}
