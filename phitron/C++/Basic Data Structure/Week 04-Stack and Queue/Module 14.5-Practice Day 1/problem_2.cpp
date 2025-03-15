c9f23a5f145af79c085d44a9bf66bf21c137487a
Anik-36 <delighteraranik@gmail.com>
2025-03-15 21:08:25 +0600

    problem 2 question is added

---
+++ phitron/C++/Basic Data Structure/Week 04-Stack and Queue/Module 14.5-Practice Day 1/problem_2.cpp
@@ -0,0 +1,21 @@
+/*Question: Take a stack of size N and a queue of size M as input. Then check if both of them are the same or not in the order of removing. You should use STL to solve this problem.
+
+Sample Input
+Sample Output
+5
+10 20 30 40 50
+5
+10 20 30 40 50
+NO
+5
+10 20 30 40 50
+4
+10 20 30 40
+NO
+5
+10 20 30 40 50
+5
+50 40 30 20 10
+YES
+
+*/
