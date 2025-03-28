09c750c30dcfadb75171ea8b54322ab930c51d88
Anik-36 <delighteraranik@gmail.com>
2025-03-28 12:35:57 +0600

    code360_inOrder_traversal is solved

---
+++ phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 18.5-Practice Day 1/code360_inOrder_traversal.cpp
@@ -0,0 +1,26 @@
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+vector<int> v;
+void inOrder(TreeNode * root){
+    if(root == NULL) return;
+    inOrder(root->left);
+    v.push_back(root->data);
+    inOrder(root->right);
+}
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    v.clear();
+    inOrder(root);
+    return v;
+}
