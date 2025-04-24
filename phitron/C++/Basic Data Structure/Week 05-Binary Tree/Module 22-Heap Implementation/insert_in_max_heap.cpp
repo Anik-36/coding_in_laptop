84d5332124baf93aa693d4babd312013748e0712
Anik-36 <delighteraranik@gmail.com>
2025-04-24 13:42:05 +0600

    insert in max heap implementation with inserti is done

--- phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 22-Heap Implementation/insert_in_max_heap.cpp
+++ phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 22-Heap Implementation/insert_in_max_heap.cpp
@@ -5,20 +5,24 @@
 #define no cout<<"NO"<<endl;
 using namespace std;
 int main(){
-    vector<int> v = {50,40,45,20,25,41,42,8,9,10};
-    int x;
-    cin>>x;
-    v.push_back(x);
-    int cur_idx = v.size()-1;
-    int par_idx = (cur_idx -1)/2;
-    while(cur_idx != 0){
+    vector<int> v ;
+    int n; cin>>n;
+    for(int i = 0; i<n; i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+        int cur_idx = v.size()-1;
         int par_idx = (cur_idx -1)/2;
-        if(v[cur_idx] > v[par_idx]){
-            swap(v[cur_idx],v[par_idx]);
-            cur_idx = par_idx;
-        }else{
-            break;
+        while(cur_idx != 0){
+            int par_idx = (cur_idx -1)/2;
+            if(v[cur_idx] > v[par_idx]){
+                swap(v[cur_idx],v[par_idx]);
+                cur_idx = par_idx;
+            }else{
+                break;
+            }
         }
     }
+
     for( int val : v) cout<<val<<" ";
 }
