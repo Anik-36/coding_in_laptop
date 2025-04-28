2f66b9bd33956838953a03b4d7b3be07e115d094
Anik-36 <delighteraranik@gmail.com>
2025-04-28 23:21:06 +0600

    setting the code formation

--- phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 22.5-Practice Day 1/1464_LTC_max_product_of_two_element_in_array.cpp
+++ phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 22.5-Practice Day 1/1464_LTC_max_product_of_two_element_in_array.cpp
@@ -5,12 +5,12 @@ https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/descri
 */
 
 class Solution {
-    public:
-        int maxProduct(vector<int>& v) {
-            sort(v.begin(),v.end());
-            int mx = v[v.size()-1] - 1;
-            v.pop_back();
-            mx = mx*(v[v.size()-1] - 1 ) ;
-            return mx;
-        }
-    };
+public:
+    int maxProduct(vector<int>& v) {
+        sort(v.begin(),v.end());
+        int mx = v[v.size()-1] - 1;
+        v.pop_back();
+        mx = mx*(v[v.size()-1] - 1 ) ;
+        return mx;
+    }
+};
