a14997e1e598ed1f933925a18cc540bbb1ced081
Anik-36 <delighteraranik@gmail.com>
2025-04-30 16:18:11 +0600

    basic of stl of map

--- phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 23-STL Priority Queue,Map,set/4.stl_map.cpp
+++ phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 23-STL Priority Queue,Map,set/4.stl_map.cpp
@@ -6,11 +6,28 @@
 using namespace std;
 int main(){
     map<string,int> mp;
-    mp.insert({"sakib al hasan",75});
-    mp.insert({"tamim iqbal",28});
-    mp.insert({"shamim hossain patowary",19});
+
+    mp["anik"] = 36; // O(log(N))
+    mp["jemin"] = 40;
+
+    // // inserting
+    // mp.insert({"sakib al hasan",75});
+    // mp.insert({"tamim iqbal",28});
+    // mp.insert({"shamim hossain patowary",19});
 
     for(auto it = mp.begin(); it!=mp.end(); it++){
-        cout<<it->first<<" "<<it->second<<endl; // though "it" is a pointer so it can't take (.), it takes (->).
+        cout<<it->first<<" "<<it->second<<endl; // O(log(N)) for single value
+        // though "it" is a pointer so it can't take (.), it takes (->).
+
+    }
+
+    cout<<mp["anik"]<<endl; //it shows the value of index anik.
+
+    //cout<<mp["anik"]<<endl; // if the index is not valid it gives 0. so for make sure we use count function .
+
+    if(mp.count("anik")){
+        cout<<"ase"<<endl;
+    }else{
+        cout<<"nai"<<endl;
     }
 }
