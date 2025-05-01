6a7c85459a2c748c251e95bafd10a87a447dec29
Anik-36 <delighteraranik@gmail.com>
2025-05-01 12:37:31 +0600

    note for set

--- phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 23-STL Priority Queue,Map,set/6.set.cpp
+++ phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 23-STL Priority Queue,Map,set/6.set.cpp
@@ -5,6 +5,9 @@
 #define no cout<<"NO"<<endl;
 using namespace std;
 int main(){
+    /* set is a bst. and it gives the value in In-Order formate that means lower to higher. it removes the duplicate values.
+
+    it makes operation in balanced bst. so all the operation will be in O(logn)*/
     set<int> s;
     int n;
     cin>>n;
@@ -13,7 +16,17 @@ int main(){
         cin>>x;
         s.insert(x); // O(logN)
     }
-    for(auto it = s.begin(); it!=s.end(); it++){
-        cout<<*it<<endl;
+
+
+    cout<<s.count(100)<<endl; // it actually gives the count but set removes all the duplicate values. so the count is always 1 for the present number.
+    if(s.count(100)){
+        cout<<"Ase"<<endl;
+    }else{
+        cout<<"Nai"<<endl;
     }
+
+
+    // for(auto it = s.begin(); it!=s.end(); it++){
+    //     cout<<*it<<endl;
+    // }
 }
