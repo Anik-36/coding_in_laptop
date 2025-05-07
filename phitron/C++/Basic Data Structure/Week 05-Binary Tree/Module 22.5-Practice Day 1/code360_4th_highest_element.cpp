5529a877ac288b3633d42acb578dd1433f62a660
Anik-36 <delighteraranik@gmail.com>
2025-05-07 14:47:05 +0600

    code360 4th highest element is solved

--- phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 22.5-Practice Day 1/code360_4th_highest_element.cpp
+++ phitron/C++/Basic Data Structure/Week 05-Binary Tree/Module 22.5-Practice Day 1/code360_4th_highest_element.cpp
@@ -1,4 +1,23 @@
-//https://www.naukri.com/code360/problems/fourth-largest-element-in-the-array_1792782#include<bits/stdc++.h>
+//https://www.naukri.com/code360/problems/fourth-largest-element-in-the-array_1792782#include<bits/stdc++.h>#include<bits/stdc++.h>
+int getFourthLargest(int arr[], int n)
+{
+    // Write your code here.
+    if(n<4) return -2147483648;
+    queue<int> q;
+    sort(arr,arr+n,greater<int>());
+    for(int i = 0; i<n; i++){
+        q.push(arr[i]);
+    }
+    for(int i = 0 ; i<3; i++){
+        q.pop();
+    }
+    return q.front();
+}
+
+
+/* raw solution
+
+
 #define endl '\n'
 #define ll long long
 #define yes cout<<"YES"<<endl;
@@ -29,3 +48,5 @@ int main(){
     cout<<q.front()<<endl;
     return 0;
 }
+
+*/
