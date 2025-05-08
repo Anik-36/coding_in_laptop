9dccdc6560f68e26c6175cf4fe77006e10e22c61
Anik-36 <delighteraranik@gmail.com>
2025-05-08 18:04:46 +0600

    revising the vector stl

--- phitron/C++/Basic Data Structure/Week 01-Introduction/Module 2-STL Vector/3.vector_insert.cpp
+++ phitron/C++/Basic Data Structure/Week 01-Introduction/Module 2-STL Vector/3.vector_insert.cpp
@@ -8,7 +8,7 @@ int main(){
     vector<int> v = {10,20,30,40};
     // v.insert(v.begin()+2,100);
     vector<int> x = {101,102,103,104,105};
-    v.insert(v.begin()+2,x.begin(),x.end());
+    v.insert(v.begin()+2,x.begin(),x.end()-1);
     for(int x: v){
         cout<<x<<" ";
     }
