1f9a488a744d9078325f280bd734cfcae9422bf0
Anik-36 <delighteraranik@gmail.com>
2025-05-16 21:50:10 +0600

    order of N complexity

---
+++ phitron/C++/Algorithm/Module 1-Introduction to Algorithm/O(N).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    for(int i = 0 ; i<n; i++){
+        // some statement
+        // the complexity is O(N)
+    }
+}
