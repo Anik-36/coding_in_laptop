29c0171eb96b0cce408deb21f933a6a26f202336
Anik-36 <delighteraranik@gmail.com>
2025-05-16 22:12:23 +0600

    order of logn

---
+++ phitron/C++/Algorithm/Module 1-Introduction to Algorithm/O(log(N)).cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    for(int i = 1; i<n; i*=2){
+        // some statement
+    }
+}
+
+/*
+    i   i*2
+    1   2
+    2   4
+    3   8
+    4   16
+    5   32
+    ...
+    k+1  2^k
+
+    2^k = n
+    k = log2^n ~= log(n)
+
+    time complexity = O(log(N))
+
+*/
