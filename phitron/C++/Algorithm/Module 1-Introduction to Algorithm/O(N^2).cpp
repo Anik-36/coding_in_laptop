f9edc9c2182dc3fa156e99490d7429c7e3cd6e7b
Anik-36 <delighteraranik@gmail.com>
2025-05-16 21:51:32 +0600

    order of N^2 complexity

---
+++ phitron/C++/Algorithm/Module 1-Introduction to Algorithm/O(N^2).cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+int main(){
+    int n;
+    for(int i = 0; i<n; i++){
+        for(int j= 0; j<n; i++){
+            // some statement
+            // complexity O(N^2)
+        }
+    }
+}
