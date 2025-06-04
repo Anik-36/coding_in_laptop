ca87b3d4729155df370b75452e11f42cb66b38af
Anik-36 <delighteraranik@gmail.com>
2025-06-04 23:18:24 +0600

    2nd largest element problem is solved

---
+++ phitron/C++/Algorithm/Week-01-Introduction/Module 3.5-Practice Day 2/Code360_2nd_largest_in_arrray.cpp
@@ -0,0 +1,54 @@
+/*
+https://www.naukri.com/code360/problems/second-largest-element-in-the-array_873375?leftPanelTab=0&leftPanelTabValue=PROBLEM
+*/
+#include <bits/stdc++.h>
+    void merge(vector<int>& a,int l,int r,int mid){
+        int left_size = mid - l+1;
+        int L[left_size+1];
+        int right_size = r-mid;
+        int R[right_size+1];
+
+        for(int i = 0,j = l; j<=mid ; i++,j++){
+            L[i] = a[j];
+        }
+        L[left_size] = INT_MIN;
+        for(int i = 0, j = mid+1; j<=r ; i++,j++){
+            R[i] = a[j];
+        }
+        R[right_size] = INT_MIN;
+
+        int lp = 0, rp = 0;
+        for(int i = l ; i<=r; i++){
+            if(L[lp]>=R[rp]){
+                a[i] = L[lp];
+                lp++;
+            }else{
+                a[i] = R[rp];
+                rp++;
+            }
+        }
+    }
+    void merge_sort(vector<int>& a,int l, int r){
+        if(l == r ) return;
+        int mid = (l+r)/2;
+        merge_sort(a,l,mid);
+        merge_sort(a,mid+1,r);
+        merge(a,l,r,mid);
+    }
+int findSecondLargest(int n, vector<int> &a)
+{
+    // Write your code here.
+    merge_sort(a,0,n-1);
+
+    // for(int i = 0; i<n; i++){
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<endl;
+    int mx = a[0];
+    for(int i = 1; i<n; i++){
+        if(a[i]!= mx){
+            return a[i];
+        }
+    }
+    return -1;
+}
