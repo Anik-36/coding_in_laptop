a9170bf408ce9ea1542463aac3ceb5b61d36f414
Anik-36 <delighteraranik@gmail.com>
2025-06-04 17:44:53 +0600

    practicing the merge sort

--- phitron/C++/Algorithm/Week-01-Introduction/Module 03-Divide and Conqueror/practice_merge_sort.cpp
+++ phitron/C++/Algorithm/Week-01-Introduction/Module 03-Divide and Conqueror/practice_merge_sort.cpp
@@ -6,21 +6,25 @@
 using namespace std;
 const int N = 1e5+10;
 int a[N];
+
 void merge(int l, int r, int mid){
     int left_size = mid - l +1;
     int L[left_size+1];
     int right_size = r-mid;
     int R[right_size+1];
 
-    for(int i = l,j = 0; i<=mid; i++,j++){
-        L[j] = a[i];
-    }
-    for(int i = mid+1,j = 0; i<=r; i++,j++){
-        R[j] = a[i];
+    for(int i = 0, j = l; j<=mid; i++,j++){
+        L[i] = a[j];
     }
     L[left_size] = INT_MAX;
+
+    for(int i = 0, j = mid+1; j<=r; i++,j++){
+        R[i] = a[j];
+    }
     R[right_size] = INT_MAX;
-    int lp = 0, rp = 0;
+
+    int lp = 0 , rp = 0;
+
     for(int i = l; i<=r; i++){
         if(L[lp]<=R[rp]){
             a[i] = L[lp];
@@ -30,13 +34,12 @@ void merge(int l, int r, int mid){
             rp++;
         }
     }
-
 }
-void mergesort(int l,int r){
-    if(l==r) return;
+void merge_sort(int l, int r){
+    if(l == r) return;
     int mid = (l+r)/2;
-    mergesort(l,mid);
-    mergesort(mid+1,r);
+    merge_sort(l,mid);
+    merge_sort(mid+1,r);
     merge(l,r,mid);
 }
 int main(){
@@ -45,9 +48,9 @@ int main(){
     for(int i = 0; i<n; i++){
         cin>>a[i];
     }
-
-    mergesort(0,n-1);
+    merge_sort(0,n-1);
     for(int i = 0; i<n; i++){
         cout<<a[i]<<" ";
     }
+
 }
