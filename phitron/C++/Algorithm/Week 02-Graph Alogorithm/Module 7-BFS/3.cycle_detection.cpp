c3d93c68f23aaf156445f5cd83ba6cde4f22071a
Anik-36 <delighteraranik@gmail.com>
2025-06-09 12:44:56 +0600

    cycle detection

---
+++ phitron/C++/Algorithm/Week 02-Graph Alogorithm/Module 7-BFS/3.cycle_detection.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+const int N = 1e3+10;
+vector<int> a[N];
+bool visited[N];
+
+void dfs(int u, int p = -1){
+    bool isCycle = false;
+    visited[u] = true;
+    for(int v: a[u]){
+        if(v == p) continue;
+        if(visited[v]) return true;
+        isCycle = isCycle | dfs(v);
+    }
+    return isCycle;
+}
+void bfs(int s){
+    visited[s] = true;
+    queue<int> q;
+    q.push(s);
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+
+        for(int v : a[u]){
+            if(visited[v]) continue;
+            q.push(v);
+            visited[v] = true;
+        }
+    }
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i =1; i<=m; i++){
+        int u,v;
+        cin>>u>>v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+
+    bool isCycle = false;
+    for(int i = 1; i<=n; i++){
+        if(visited[i]) continue;
+        isCycle = isCycle or dfs(i);
+    }
+    if(isCycle){
+        cout<<"Cycle detected !"<<endl;
+    }else{
+        cout<<"No cycle detected !"<<endl;
+    }
+}
