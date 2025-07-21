561446190a522c6ce6ba6949ae8434b151d5228d
Anik-36 <delighteraranik@gmail.com>
2025-07-21 22:39:34 +0600

    distance problem is solved

--- phitron/C++/Algorithm/Week 02-Graph Alogorithm/Module 8-Assignment 2/Distance.cpp
+++ phitron/C++/Algorithm/Week 02-Graph Alogorithm/Module 8-Assignment 2/Distance.cpp
@@ -11,8 +11,7 @@ const int N = 1e5+10;
 vector<int> adlist[N];
 bool visited[N];
 int level[N];
-bool bfs(int s, int d){
-    bool flag = false;
+void bfs(int s){
     visited[s] =true;
     level[s] = 0;
     queue<int> q;
@@ -21,23 +20,24 @@ bool bfs(int s, int d){
         int u = q.front();
         q.pop();
         for(int v : adlist[u]){
-            if(v == d) flag = true;
             if(visited[v]) continue;
             visited[v] = true;
             level[v] = level[u] + 1;
             q.push(v);
         }
     }
-    return flag;
 }
 int main(){
     int n,m;
     cin>>n>>m;
+    set<int> s;
     for(int i = 1; i<=m; i++){
         int u,v;
         cin>>u>>v;
         adlist[u].push_back(v);
         adlist[v].push_back(u);
+        s.insert(u);
+        s.insert(v);
     }
     int q;
     cin>>q;
@@ -46,12 +46,14 @@ int main(){
         memset(level,0,N);
         int u,v;
         cin>>u>>v;
-
-        if(u<n and v<n){
-            bool flag = bfs(u,v);
-            if(flag){
-                int ans = abs(level[u] - level[v]);
-                cout<<ans<<endl;
+        if(u == v){
+            cout<<"0"<<endl;
+            continue;
+        }
+        else if(s.find(u)!=s.end() && s.find(v)!=s.end()){
+            bfs(u);
+            if(visited[v]){
+                cout<<level[v]<<endl;
             }else{
                 cout<<"-1"<<endl;
             }
@@ -60,3 +62,77 @@ int main(){
         }
     }
 }
+
+
+
+
+/*
+
+#include<bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+const int N = 1e5+5;
+vector<int> adlist[N];
+bool visited[N];
+int level[N];
+
+void bfs(int s){
+    visited[s] = true;
+    level[s] = 0;
+    queue<int> q;
+    q.push(s);
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        for(int v : adlist[u]){
+            if(visited[v]) continue;
+            visited[v] = true;
+            level[v] = level[u] + 1;
+            q.push(v);
+        }
+    }
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    set<int> s;
+    for(int i = 1; i<=m; i++){
+        int u,v;
+        cin>>u>>v;
+        adlist[u].push_back(v);
+        adlist[v].push_back(u);
+        s.insert(u);
+        s.insert(v);
+    }
+    vector<int> vec;
+    for(auto x : s) vec.push_back(x);
+    memset(visited,false,sizeof(visited));
+    memset(level,0,sizeof(level));
+    int q;
+    cin>>q;
+    while(q--){
+        int u,v;
+        cin>>u>>v;
+        if(u == v){
+            cout << 0 << endl;
+            continue;
+        }
+        else if(s.find(u) != s.end() && s.find(v) != s.end()) bfs(u);
+        else{
+            cout << -1 << endl;
+            continue;
+        }
+        if(visited[v]){
+            cout << level[v] << endl;
+        }
+        else cout << -1 << endl;
+        for(auto x : vec){
+            visited[x] = false;
+            level[x] = 0;
+        }
+    }
+}
+    */
