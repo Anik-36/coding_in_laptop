d2ed1fd4efee94d8e69c86121f4477b07579197f
Anik-36 <delighteraranik@gmail.com>
2025-07-30 21:07:21 +0600

    labyrinth problem is sovled

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -10,11 +10,36 @@ typedef pair<int,int> pii;
 int visited[N][N];
 int level[N][N];
 pii parrent[N][N];
+int n,m;
 vector<pii> direc = {{0,1},{0,-1},{1,0},{-1,0}};
+bool isValid(int i ,int j){
+    return (i>=0 and i<n and j>=0 and j<m);
+}
+void bfs(int si,int sj){
+    queue<pii> q;
+    q.push({si,sj});
+    visited[si][sj] = true;
+    level[si][sj] = 0;
+    while(!q.empty()){
+        pii upair = q.front();
+        int i = upair.first;
+        int j = upair.second;
+        q.pop();
+        for(auto d:direc){
+            int ni= d.first+i;
+            int nj= d.second+j;
+            if(isValid(ni,nj) and !visited[ni][nj] and g[ni][nj] != '#'){
+                q.push({ni,nj});
+                visited[ni][nj] = true;
+                level[ni][nj] = level[i][j]+1;
+                parrent[ni][nj] = {i,j};
+            }
+        }
+    }
+}
 int main(){
-    int n,m;
     cin>>n>>m;
-    int si,sf,di,dj;
+    int si,sj,di,dj;
     for(int i = 0; i<n; i++){
         string x;
         cin>>x;
@@ -26,9 +51,37 @@ int main(){
                 di = i, dj = j;
             }
         }
-        g.push(x);
+        g.push_back(x);
     }
 
     bfs(si,sj);
-    cout<<level[di][dj]<<endl;
+    if(level[di][dj]!=0){
+        cout<<"YES"<<endl;
+        cout<<level[di][dj]<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    vector<pii> path;
+    pii curr = {di,dj};
+    while(!(curr.first == si and curr.second == sj)){
+        path.push_back(curr);
+        curr = parrent[curr.first][curr.second];
+    }
+    path.push_back({si,sj});
+    reverse(path.begin(),path.end());
+    for(int i = 1; i<path.size(); i++){
+        if(path[i-1].first == path[i].first){
+            if(path[i-1].second == path[i].second-1){
+                cout<<"R";
+            }else{
+                cout<<"L";
+            }
+        }else{
+            if(path[i-1].first == path[i].first-1){
+                cout<<"D";
+            }else{
+                cout<<"U";
+            }
+        }
+    }
 }
