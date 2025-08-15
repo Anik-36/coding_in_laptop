249af120e4d1fb8814d47379ff176efdda87c0a4
Anik-36 <delighteraranik@gmail.com>
2025-08-15 22:07:44 +0600

    methods of list is done

--- python by Harry/chapter-4/01_list.py
+++ python by Harry/chapter-4/01_list.py
@@ -2,3 +2,4 @@ friends = ["Apple","Orange",5,3.58,False,"Akash"]
 print(friends[0])
 friends[0] = "Grapes" # Unlike string, lists are mutable. we can change list  but not string
 print(friends[0])
+print(friends[1:4])
---
+++ python by Harry/chapter-4/02_list_methods.py
@@ -0,0 +1,13 @@
+friends = ["Apple","Orange",5,3.58,False,"Akash"]
+print(friends )
+friends.append("Harry")
+print(friends)
+
+l1 = [4,6,1,2,10,3]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3,33333) #insert 33333 at index 3 in the list
+# value = l1.pop(2)
+# print(value)
+l1.remove(10)
+print(l1)
