ac85cb7a74bf7d1ee4da3b6830831b35feec6a90
Anik-36 <delighteraranik@gmail.com>
2025-08-15 18:33:32 +0600

    learn the negative slicing

---
+++ python/chapter-4/01_intro_to_string.py
@@ -0,0 +1,4 @@
+name = "anisur"
+
+nameshort = name[0:3] # start from 0 all the way till 3 (excluding 3)
+print(nameshort)
---
+++ python/chapter-4/02_negative_slicing.py
@@ -0,0 +1,10 @@
+name = "anisur"
+
+print(name[-4:-1])
+
+print(name[2:5])
+print(name[2:len(name)])
+print(len(name))
+
+print(name[:6])
+print(name[0:])
