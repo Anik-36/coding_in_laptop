91e981ccd7815eecc3b02249e3a83cb2cc7a630f
Anik-36 <delighteraranik@gmail.com>
2025-08-15 19:32:38 +0600

    using replace function

---
+++ python/chapter-4/04_str_function.py
@@ -0,0 +1,5 @@
+name = "harry"
+print(len(name))
+print(name.endswith("rry")) # give the true or false value
+print(name.startswith("ha"))
+print(name.capitalize())
---
+++ python/chapter-4/05_replace_function.py
@@ -0,0 +1,3 @@
+name = "harry is a good good boy"
+a = name.replace("good","bad")
+print(a)
