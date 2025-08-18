80801e555db2e677384a08cf3124f151477b5f14
Anik-36 <delighteraranik@gmail.com>
2025-08-18 21:48:20 +0600

    chapter 4 list problem 1 is solved

---
+++ python by Harry/chapter-4_PS/problem1.py
@@ -0,0 +1,18 @@
+fruits = [];
+
+f1 = input("Enter the fruit name : ")
+fruits.append(f1)
+f2 = input("Enter the fruit name : ")
+fruits.append(f2)
+f3 = input("Enter the fruit name : ")
+fruits.append(f3)
+f4 = input("Enter the fruit name : ")
+fruits.append(f4)
+f5 = input("Enter the fruit name : ")
+fruits.append(f5)
+f6 = input("Enter the fruit name : ")
+fruits.append(f6)
+f7 = input("Enter the fruit name : ")
+fruits.append(f7)
+
+print(fruits)
