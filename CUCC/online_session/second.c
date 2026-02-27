a1d685c266aa8b61e34f069ce43d0b7d59c02577
Anik-36 <delighteraranik@gmail.com>
2026-02-27 19:59:28 +0600

    taking 2nd class of cupc

---
+++ CUCC/online_session/second.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+int main(){
+    // int studentIdForUniversity;
+    // // int student_id ;
+    // int a = 1e9;
+    // // int a = 1000000000;
+    // printf("%d\n",a);
+    // float b = 5.6;
+    // char c = 'a';
+    // printf("%c",c);
+    // long long int d;
+    // double
+
+    // printf("%zu\n",sizeof(int));
+    // printf("%zu\n",sizeof(float));
+    // printf("%zu\n",sizeof(double));
+    // printf("%zu\n",sizeof(char));
+    // int a = 5;
+    // int b = 2;
+    // int c =  5/2;
+    // // float c = (float) a/b;  // 2
+
+    // // float d = 5;
+
+    // printf("%f\n", (float)c);
+
+
+    // // printf("%f", d);
+
+
+    // const int SUN = 3*1e8;
+
+    // printf("%d",sun);
+
+    // int a = 3;
+    // float b = 2.5;
+    // float c = (float) a*b;
+    // printf("%f",c);
+
+    // int a = 4;
+    // a = a+1;
+    // a++; // incremental operator
+    // printf("%d\n",a);
+
+    // int  b= 5;
+    // // b = b-2;
+    // // b-=2;
+    // // b+=2;
+    // // b = b*2;
+    // b*=2;
+    // printf("%d",b);
+
+    // int a = 2;
+    // // printf("%d\n",a++);  /// 2 -> a = 2+1 = 3
+
+    // // printf("%d\n",a);
+
+    // printf("%d\n",++a);
+
+
+    // int a = 5;
+    // // int b= 2 ;
+
+    // a = a+5;
+    // a+=5;
+
+    // a = a%2;
+    // a%=2;
+
+
+    // printf("%d", a);
+
+    // int a = 5;
+    // int b=  2;
+
+    // condition1 && condition2
+
+
+
+    return 0;
+}
+
+// 1e9 - 1e12
+// >1e12
+
+/*
+operator
+	-arithmetic - +,-,*,/,++,--
+	-assignment - =,+=,-=,*=,/=,%=
+	-comparison - ==,!=,>,<,>=,<=
+	-logical - &&,||,!
+
+
+*/
